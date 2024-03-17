0f65ac62e4bb4d98de00c57d4b6c8fd8d7398002
teresakramek <teresakramek@interia.pl>
2024-03-17 20:20:25 +0100

    FinishedVersionTopicFourth

---
+++ unit-testing-using-nunit/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using Prime.Service;
+
+namespace Prime.UnitTests.Services
+{
+    [TestFixture]
+    public class PrimeService_IsPrimeShould
+    {
+        private PrimeService _primeService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService?.IsPrime(value);
+
+            Assert.That(result, Is.False, $"{value} should not be prime");
+        }
+
+    }
+}
---
+++ unit-testing-using-nunit/PrimeService/PrimeService.cs
@@ -0,0 +1,13 @@
+﻿namespace Prime.Service;
+public class PrimeService
+{
+    public bool IsPrime(int candidate)
+{
+    if (candidate < 2)
+    {
+        return false;
+    }
+    throw new NotImplementedException("Please create a test first.");
+}
+
+}
